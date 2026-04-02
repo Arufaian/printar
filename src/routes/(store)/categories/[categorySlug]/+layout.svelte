6413cfcf84e4f36d07d41af50f0a6abb76e5a46e
Arufaian <alfianilyaysa13@gmail.com>
2026-04-02 18:37:57 +0700

    feat: add new category page

---
+++ src/routes/(store)/categories/[categorySlug]/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<section class="mx-auto w-full">
+	{@render children()}
+</section>
---
+++ src/routes/(store)/categories/[categorySlug]/+page.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { ChevronDown, Funnel, ArrowUpDown, Star, ShoppingCart } from '@lucide/svelte/icons';
+
+	import * as Card from '$lib/components/ui/card/index.js';
+
+	let items = Array.from({ length: 10 });
+</script>
+
+<div class="container mx-auto px-4 py-8 lg:px-8">
+	<div class="flex flex-col justify-between">
+		<div class="mb-8">
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Item>
+						<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator />
+					<Breadcrumb.Item>
+						<Breadcrumb.Link href="/components">Components</Breadcrumb.Link>
+					</Breadcrumb.Item>
+					<Breadcrumb.Separator />
+					<Breadcrumb.Item>
+						<Breadcrumb.Page>Breadcrumb</Breadcrumb.Page>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</div>
+
+		<div class="mb-8">
+			<img class="rounded-md object-center" src="https://picsum.photos/1344/420" alt="" />
+		</div>
+
+		<div class="mb-16">
+			<p class="text-sm leading-relaxed text-muted-foreground md:text-base">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum adipisci quos fuga maxime!
+				Consequuntur autem, quos nulla sapiente necessitatibus perferendis tempora error eligendi
+				eius ratione ducimus accusantium placeat, iusto neque.
+			</p>
+		</div>
+
+		<div class="mb-8 flex justify-between">
+			<div class="flex items-baseline gap-2">
+				<span class="text-sm font-semibold md:text-base">Merch</span>
+				<span class="text-xs text-muted-foreground md:text-sm">(120 produk)</span>
+			</div>
+
+			<div class="flex gap-4">
+				<Button variant="outline" class="px-4 py-6 text-sm md:text-base">
+					<ArrowUpDown class="size-5 stroke-1" />
+					<span>Urutkan</span>
+					<ChevronDown class="size-5 stroke-1" /></Button
+				>
+				<Button variant="outline" class="px-4 py-6 text-sm md:text-base">
+					<Funnel class="size-5 stroke-1" />
+					<span>Filter</span>
+					<ChevronDown class="size-5 stroke-1" /></Button
+				>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-5">
+			{#each items as _, i (i)}
+				<Card.Root>
+					<img
+						src="https://picsum.photos/265/265"
+						alt="product"
+						class="aspect-square object-cover"
+					/>
+
+					<Card.Header class="m-0 flex flex-col items-center gap-0 p-0">
+						<Card.Title
+							role="heading"
+							aria-level={3}
+							class="text-xs leading-tight font-semibold sm:text-sm md:text-base"
+							>Product Title</Card.Title
+						>
+					</Card.Header>
+
+					<Card.Content>
+						<div class="mb-2 flex justify-center">
+							<span class="text-sm font-bold text-primary sm:text-base md:text-lg"
+								>RP. 1{_}0.000</span
+							>
+						</div>
+						<div class="flex justify-between">
+							<div class="flex items-center gap-1">
+								<Star class="size-4 fill-primary text-primary" />
+								<span class="text-xs font-medium sm:text-sm md:text-base">4.5</span>
+							</div>
+							<span class="text-xs text-muted-foreground sm:text-sm">(120 terjual)</span>
+						</div>
+					</Card.Content>
+
+					<Card.Footer>
+						<Button class="w-full">
+							<ShoppingCart />
+							<span>Checkout</span>
+						</Button>
+					</Card.Footer>
+				</Card.Root>
+			{/each}
+		</div>
+	</div>
+</div>
