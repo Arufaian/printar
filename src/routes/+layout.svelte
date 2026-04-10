dea94a3c85d483aca73d7701c30e547226985e8a
Arufaian <alfianilyaysa13@gmail.com>
2026-04-10 15:24:47 +0700

    chore: add package svelte-sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import './layout.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	// import { Toaster } from '$lib/components/ui/sonner';
+	import { Toaster } from '$lib/components/ui/sonner';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -11,4 +12,5 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
+
 {@render children()}
