e33c266a1c42bbba795227336f83c8b3ad31f2dc
Arufaian <alfianilyaysa13@gmail.com>
2026-04-23 17:27:35 +0700

    feat: add slug to products table

--- src/lib/server/db/schema/product.ts
+++ src/lib/server/db/schema/product.ts
@@ -5,6 +5,7 @@ export const products = pgTable('products', {
 	id: uuid('id').defaultRandom().primaryKey(),
 	categoryId: uuid('category_id').references(() => categories.id),
 	name: text('name'),
+	slug: text('slug').unique(),
 	description: text('description'),
 	createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
 	deletedAt: timestamp('deleted_at', { withTimezone: true })
