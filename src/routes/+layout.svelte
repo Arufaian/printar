6ddd1cf2dbf6f0724206b23e03c266595d8ff158
Arufaian <alfianilyaysa13@gmail.com>
2026-04-26 13:29:08 +0700

    feat(ui): make route loading overlay global from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import './layout.css';
+	import { navigating } from '$app/state';
 	import { ModeWatcher } from 'mode-watcher';
 	// import { Toaster } from '$lib/components/ui/sonner';
+	import { LoadingOverlay } from '$lib/components/ui/loading-overlay/index.js';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -9,6 +11,10 @@
 </script>
 
 <Toaster richColors />
+
+{#if navigating.to}
+	<LoadingOverlay />
+{/if}
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
